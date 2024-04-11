309b1dadbab7a17934d619cb77b54c4406891473
chunlistudiobrasuka <chunli@studiobrasuka.com.br>
2024-04-11 11:03:19 -0300

    teste

--- Bsk.Solution/Bsk.Site/Cliente/negociar-cotacao.aspx.designer.cs
+++ Bsk.Solution/Bsk.Site/Cliente/negociar-cotacao.aspx.designer.cs
@@ -102,15 +102,6 @@ namespace Bsk.Site.Cliente
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divAceitar;
-
-        /// <summary>
-        /// Controle descricaoHide.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl descricaoHide;
 
         /// <summary>
