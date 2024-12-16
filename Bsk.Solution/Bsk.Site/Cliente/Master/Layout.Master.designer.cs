f95fe9571c96c5398be71dddc6f4c9968074d94d
chunlistudiobrasuka <chunli@studiobrasuka.com.br>
2024-12-16 17:19:16 -0300

    menu encima mobile

--- Bsk.Solution/Bsk.Site/Cliente/Master/Layout.Master.designer.cs
+++ Bsk.Solution/Bsk.Site/Cliente/Master/Layout.Master.designer.cs
@@ -31,5 +31,14 @@ namespace Bsk.Site.Cliente.Master
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder conteudo;
+
+        /// <summary>
+        /// divAceitar3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl perfilMobile;
     }
 }
