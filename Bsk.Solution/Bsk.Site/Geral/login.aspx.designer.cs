29a7bb9c2530ca96b437344ba79c0d064676da57
kenstudiobrasuka <ken@studiobrasuka.com.br>
2025-01-23 11:22:45 -0300

    commit rafael 23/01

--- Bsk.Solution/Bsk.Site/Geral/login.aspx.designer.cs
+++ Bsk.Solution/Bsk.Site/Geral/login.aspx.designer.cs
@@ -60,6 +60,15 @@ namespace Bsk.Site.Geral
         protected global::System.Web.UI.WebControls.Label lblMsg;
 
         /// <summary>
+        /// Controle btnMensagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnMensagem;
+
+        /// <summary>
         /// Controle btnCliente.
         /// </summary>
         /// <remarks>
